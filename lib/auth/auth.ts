395f169a18a2e302bf6dc077c63f1d192d1ba8ba
ancientphoenix34 <farsukoothradan@gmail.com>
2026-03-09 05:01:05 +0530

    add auth and mongodb connection

--- lib/auth/auth.ts
+++ lib/auth/auth.ts
@@ -1,6 +1,8 @@
 import { betterAuth } from "better-auth";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 import { MongoClient } from "mongodb";
+import dns from "node:dns/promises";
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
 
 const client = new MongoClient(process.env.MONGODB_URI!);
 const db = client.db()
